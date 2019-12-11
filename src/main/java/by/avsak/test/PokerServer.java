661404309450e083fa5cd86ecaf246f2a3dc6a35
avsak <sakandrei@gmail.com>
2019-12-11 16:58:38 +0300

    Added log4j2

---
+++ src/main/java/by/avsak/test/PokerServer.java
@@ -0,0 +1,12 @@
+package by.avsak.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PokerServer {
+
+    private static final Logger log = LogManager.getRootLogger();
+    public static void main(String[] args) {
+        log.info("Server is running");
+    }
+}
