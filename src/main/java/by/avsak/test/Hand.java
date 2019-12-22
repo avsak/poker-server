b65e8fc965eae969444ea4b46b99d002ea042911
avsak <sakandrei@gmail.com>
2019-12-22 21:06:20 +0300

    wip

--- src/main/java/by/avsak/test/Hand.java
+++ src/main/java/by/avsak/test/Hand.java
@@ -1,6 +1,8 @@
 package by.avsak.test;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.counting;
@@ -36,30 +38,12 @@ public class Hand implements Comparable<Hand> {
         allCards.addAll(board.getCards());
         allCards.addAll(this.cards);
 
-        if (detectStraightFlush(allCards)) {
-            this.combinationType = CombinationType.StraightFlush;
-            return;
-        }
-        if (detectFourOfKind(allCards)) {
-            this.combinationType = CombinationType.FourOfKind;
-            return;
-        }
-        if (detectFullHouse(allCards)) {
-            this.combinationType = CombinationType.FullHouse;
-            return;
-        }
-        if (detectFlush(allCards)) {
-            this.combinationType = CombinationType.Flush;
-            return;
-        }
-        if (detectStraight(allCards)) {
-            this.combinationType = CombinationType.Straight;
-            return;
-        }
-        if (detectThreeOfKind(allCards)) {
-            this.combinationType = CombinationType.ThreeOfKind;
-            return;
-        }
+        if (detectStraightFlush(allCards)) return;
+        if (detectFourOfKind(allCards)) return;
+        if (detectFullHouse(allCards)) return;
+        if (detectFlush(allCards)) return;
+        if (detectStraight(allCards)) return;
+        if (detectThreeOfKind(allCards)) return;
         if (detectTwoPairs(allCards)) return;
         if (detectPair(allCards)) return;
 
@@ -90,34 +74,127 @@ public class Hand implements Comparable<Hand> {
     }
 
     private boolean detectStraightFlush(List<Card> cards) {
-        List<Card> cardList = new ArrayList<>(cards);
         List<Integer> straight = new ArrayList<>(Arrays.asList(14, 13, 12, 11, 10));
-        Map<CardSuit, Long> cardSuitsCount = cardList.stream().collect(groupingBy(Card::getSuit, counting()));
-        cardSuitsCount.forEach((s, c) -> {
-            if (c < 5L) {
-                cardList.removeIf(card -> card.getSuit().equals(s));
-            }
-        });
+        List<Integer> wheel = new ArrayList<>(Arrays.asList(14, 2, 3, 4, 5));
+        List<Card> cardList = new ArrayList<>(cards);
+
+        cardList = cardList.stream()
+                .filter(distinctByKey(Card::getRank))
+                .collect(Collectors.toList());
+
         if (cardList.size() < 5) return false;
 
-        Collections.sort(cardList);
-        Collections.reverse(cardList);  // A -> K -> Q -> ...
+        cardList.sort(Comparator.reverseOrder()); // Descending: A -> K -> Q -> ...
+        List<Integer> cardsRank = cardList.stream()
+                .map(card -> card.getRank().getPower())
+                .collect(Collectors.toList());
+
         while (straight.get(straight.size() - 1) > 2) {
-            if (cardList.stream().map(card -> card.getRank().getPower()).collect(Collectors.toList()).containsAll(straight)) {
-                return true;
-            } else straight = straight.stream().map(cardRank -> cardRank - 1).collect(Collectors.toList());
+            if (cardsRank.containsAll(straight)) {
+
+                Map<CardSuit, Long> suitsCount = cardList.stream()
+                        .collect(groupingBy(Card::getSuit, counting()));
+
+                if (suitsCount.containsValue(5L) || suitsCount.containsValue(6L) || suitsCount.containsValue(7L)) {
+                    this.combinationType = CombinationType.StraightFlush;
+                    this.combinationType.setCardCombinations(cardList.subList(0, 5));
+
+                    System.out.println(cardList.subList(0, 5));
+                    return true;
+                }
+
+            } else {
+                straight = straight.stream()
+                        .map(cardRank -> cardRank - 1)
+                        .collect(Collectors.toList());
+            }
         }
+
+        if (cardsRank.containsAll(wheel)) {
+            List<Card> cardCombinations = new ArrayList<>();
+
+            cardList.stream()
+                    .filter(c -> c.getRank().getPower() == 14
+                            || c.getRank().getPower() == 2
+                            || c.getRank().getPower() == 3
+                            || c.getRank().getPower() == 4
+                            || c.getRank().getPower() == 5)
+                    .forEach(cardCombinations::add);
+
+            Map<CardSuit, Long> suitsCount = cardCombinations.stream()
+                    .collect(groupingBy(Card::getSuit, counting()));
+
+            System.out.println(suitsCount);
+
+            if (suitsCount.containsValue(5L)) {
+
+                this.combinationType = CombinationType.StraightFlush;
+                this.combinationType.setCardCombinations(cardCombinations);
+                System.out.println(cardCombinations);
+                return true;
+            }
+        }
+
         return false;
     }
 
     private boolean detectFourOfKind(List<Card> cards) {
-        Map<CardRank, Long> cardRanksCount = cards.stream().collect(groupingBy(Card::getRank, counting()));
-        return cardRanksCount.containsValue(4L);
+        Map<CardRank, Long> ranksCount = cards.stream()
+                .collect(groupingBy(Card::getRank, counting()));
+
+        if (ranksCount.containsValue(4L)) {
+            List<Card> cardCombinations = new ArrayList<>();
+
+            CardRank fourCardRank = ranksCount.entrySet().stream()
+                    .filter(rc -> rc.getValue() == 4L)
+                    .map(Map.Entry::getKey)
+                    .findFirst()
+                    .orElse(null);
+
+            cards.stream()
+                    .filter(card -> card.getRank() == fourCardRank)
+                    .forEach(cardCombinations::add);
+
+            this.combinationType = CombinationType.FourOfKind;
+            this.combinationType.setCardCombinations(cardCombinations);
+            return true;
+        }
+        return ranksCount.containsValue(4L);
     }
 
     private boolean detectFullHouse(List<Card> cards) {
-        Map<CardRank, Long> cardRanksCount = cards.stream().collect(groupingBy(Card::getRank, counting()));
-        return cardRanksCount.containsValue(3L) && cardRanksCount.containsValue(2L);
+        Map<CardRank, Long> ranksCount = cards.stream()
+                .collect(groupingBy(Card::getRank, counting()));
+
+        if (ranksCount.containsValue(3L) && ranksCount.containsValue(2L)) {
+            List<Card> cardCombinations = new ArrayList<>();
+
+            CardRank threeCardRank = ranksCount.entrySet().stream()
+                    .filter(rc -> rc.getValue() == 3L)
+                    .map(Map.Entry::getKey)
+                    .findFirst()
+                    .orElse(null);
+
+            CardRank pairCardRank = ranksCount.entrySet().stream()
+                    .filter(rc -> rc.getValue() == 2L)
+                    .map(Map.Entry::getKey)
+                    .findFirst()
+                    .orElse(null);
+
+            cards.stream()
+                    .filter(card -> card.getRank() == threeCardRank)
+                    .forEach(cardCombinations::add);
+
+            cards.stream()
+                    .filter(card -> card.getRank() == pairCardRank)
+                    .forEach(cardCombinations::add);
+
+            this.combinationType = CombinationType.FullHouse;
+            this.combinationType.setCardCombinations(cardCombinations); // Ordered - [X,X,X,Y,Y]
+            return true;
+        }
+
+        return false;
     }
 
     private boolean detectFlush(List<Card> cards) {
@@ -134,7 +211,7 @@ public class Hand implements Comparable<Hand> {
 
             cards.stream()
                     .filter(card -> card.getSuit() == flushCardSuit)
-                    .sorted(Comparator.reverseOrder())
+                    .sorted(Comparator.reverseOrder()) // Descending
                     .limit(5)
                     .forEach(cardCombinations::add);
 
@@ -147,14 +224,48 @@ public class Hand implements Comparable<Hand> {
 
     private boolean detectStraight(List<Card> cards) {
         List<Integer> straight = new ArrayList<>(Arrays.asList(14, 13, 12, 11, 10));
-        Collections.sort(cards);
-        Collections.reverse(cards);  // A -> K -> Q -> ...
-        List<Integer> cardsRank = cards.stream().map(card -> card.getRank().getPower()).collect(Collectors.toList());
+        List<Integer> wheel = new ArrayList<>(Arrays.asList(14, 2, 3, 4, 5));
+        List<Card> cardList = new ArrayList<>(cards);
+
+        cardList = cardList.stream()
+                .filter(distinctByKey(Card::getRank))
+                .collect(Collectors.toList());
+
+        if (cardList.size() < 5) return false;
+
+        cardList.sort(Comparator.reverseOrder()); // Descending: A -> K -> Q -> ...
+        List<Integer> cardsRank = cardList.stream()
+                .map(card -> card.getRank().getPower())
+                .collect(Collectors.toList());
+
         while (straight.get(straight.size() - 1) > 2) {
             if (cardsRank.containsAll(straight)) {
+                this.combinationType = CombinationType.Straight;
+                this.combinationType.setCardCombinations(cardList.subList(0, 5));
                 return true;
-            } else straight = straight.stream().map(cardRank -> cardRank - 1).collect(Collectors.toList());
+            } else {
+                straight = straight.stream()
+                        .map(cardRank -> cardRank - 1)
+                        .collect(Collectors.toList());
+            }
         }
+
+        if (cardsRank.containsAll(wheel)) {
+            List<Card> cardCombinations = new ArrayList<>();
+
+            cardList.stream()
+                    .filter(c -> c.getRank().getPower() == 14
+                            || c.getRank().getPower() == 2
+                            || c.getRank().getPower() == 3
+                            || c.getRank().getPower() == 4
+                            || c.getRank().getPower() == 5)
+                    .forEach(cardCombinations::add);
+
+            this.combinationType = CombinationType.Straight;
+            this.combinationType.setCardCombinations(cardCombinations);
+            return true;
+        }
+
         return false;
     }
 
@@ -232,5 +343,15 @@ public class Hand implements Comparable<Hand> {
         }
 
         return false;
+    }
+
+
+    /**
+     * Function that returns a predicate that maintains state about what it's seen previously,
+     * and that returns whether the given element was seen for the first time
+     */
+    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = new HashSet<>();
+        return t -> seen.add(keyExtractor.apply(t));
     }
 }
